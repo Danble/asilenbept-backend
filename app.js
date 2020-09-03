057c4fc4f889dc4cf80f279bbb76e24ac5604a89
Diego Córdova <gatesofsilent@hotmail.com>
2020-09-02 20:51:27 -0500

    First tested insertion to MySQL done

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello world!")
+const mysql = require('mysql')
+
+const server = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'toor',
+  database: 'Test'
+})
+
+server.connect(err => {
+  if (err) throw err;
+  server.query(`INSERT INTO users (id, users) VALUES (?, ?)`, [2, 'probando2'], (err, result, fields) => {
+    if (err) throw err;
+    console.log(result);
+ })
+})
