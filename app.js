89de571296d6dc72600ed3848c28d16405473567
Diego Córdova <gatesofsilent@hotmail.com>
2020-10-12 00:03:39 -0500

    API is working!

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const db = require('./models')
 
@@ -6,9 +7,10 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
 
 const PORT = process.env.PORT || 8080
-require('./routes/user.routes')(app)
+require('./controller/user.controller')(app)
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
